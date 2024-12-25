d63563994c2644e40c446b128790910b78e6229b
mirupio <162397816+mirupio@users.noreply.github.com>
2024-12-25 22:52:19 +0900

    Application.java

--- src/main/java/com/gdsc/game/Application.java
+++ src/main/java/com/gdsc/game/Application.java
@@ -1,9 +1,11 @@
 package com.gdsc.game;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        System.out.println("hello world");
+        SpringApplication.run(Application.class, args);
     }
-
 }
